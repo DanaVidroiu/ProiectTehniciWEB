05c829016a338b1e6b5fddfa3ccfe09b279d9568
DanaVidroiu <113728212+DanaVidroiu@users.noreply.github.com>
2023-05-25 09:52:30 +0300

    Update PaginaGatsby.js

--- PaginaGatsby.js
+++ PaginaGatsby.js
@@ -6,29 +6,24 @@ var images = Array.from(gallerySlider.querySelectorAll('img'));
 var currentIndex = 0;
 
 
-// Ascundeți elementul inițial
+
 subtitlu.style.height = '0';
 subtitlu.style.overflow = 'hidden';
 subtitlu.style.transition = 'height 2s';
 
-// Faceți-l să apară prin animarea înălțimii
 setTimeout(function() {
-  subtitlu.style.height = '100px'; // Înălțimea dorită
+  subtitlu.style.height = '100px'; 
 }, 500);
 
 
-
-// Funcția de inițializare a galeriei
 function initGallery() {
-  // Ascunde toate imaginile inițial
   images.forEach(function(image) {
     image.style.display = 'none';
   });
 
-  // Afișează prima imagine
   images[currentIndex].style.display = 'block';
 
-  // Adaugă event listener pentru butoanele de navigare
+
   prevButton.addEventListener('click', function() {
     navigateGallery(-1);
   });
@@ -38,7 +33,7 @@ function initGallery() {
   });
 }
 
-// Funcția de navigare în galerie
+
 function navigateGallery(direction) {
   images[currentIndex].style.display = 'none';
   currentIndex += direction;
@@ -52,5 +47,4 @@ function navigateGallery(direction) {
   images[currentIndex].style.display = 'block';
 }
 
-// Apelăm funcția de inițializare a galeriei
 initGallery();
